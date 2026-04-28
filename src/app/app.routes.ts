22acb9eef0da21fdabc5c3a708468b782a0719a4
jmarkiewicz <jmarkiewicz@deloittece.com>
2026-04-28 19:45:37 +0200

    10 routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserPage } from './user-page/user-page';
+import { BooksPage } from './books-page/books-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BooksPage },
+  { path: 'user', component: UserPage },
+  { path: '**', component: UserPage },
+];
--- src/app/books-page/books-page.ts
+++ src/app/books-page/books-page.ts
@@ -1,14 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BookService } from './book.service';
-import { BookI } from './book/book';
+import { BookService } from '../book.service';
+import { Book, BookI } from '../book/book';
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrls: ['./app.css'],
+  selector: 'app-books-page',
+  imports: [Book],
+  templateUrl: './books-page.html',
+  styleUrls: ['./books-page.css'],
 })
-export class App {
+export class BooksPage {
   booksService = inject(BookService);
 
   books = signal<BookI[]>([]);
