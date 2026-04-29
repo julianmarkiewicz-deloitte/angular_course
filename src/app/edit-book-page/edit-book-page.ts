b5f92fa8a96a9969f424cc92af6b399bfc67e915
jmarkiewicz <jmarkiewicz@deloittece.com>
2026-04-29 19:14:16 +0200

    15 Reactive forms update validation

--- src/app/edit-book-page/edit-book-page.ts
+++ src/app/edit-book-page/edit-book-page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BookService } from '../book.service';
 
@@ -18,11 +18,15 @@ export class EditBookPage implements OnInit {
   bookId = '';
 
   bookForm = new FormGroup({
-    title: new FormControl(''),
-    synopsis: new FormControl(''),
-    pages: new FormControl(0),
-    price: new FormControl(0),
-    authors: new FormControl(''),
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern(/^[^0-9]*$/),
+    ]),
+    synopsis: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    pages: new FormControl(0, [Validators.required, Validators.min(1)]),
+    price: new FormControl(0, [Validators.required, Validators.max(999)]),
+    authors: new FormControl('', [Validators.required]),
   });
 
   ngOnInit() {
